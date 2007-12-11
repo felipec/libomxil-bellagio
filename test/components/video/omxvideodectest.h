03edc56fa639cf43c1ecef6d0b1fc0bd4502166e
Pankaj Sen <pankaj.sen@st.com>
2007-12-11 06:25:59 +0000

    Increasing default input buffer size
    git-svn-id: https://omxil.svn.sourceforge.net/svnroot/omxil/libomxil/trunk@388 d11a7513-620d-0410-9d76-e80dcb6a9808

--- test/components/video/omxvideodectest.h
+++ test/components/video/omxvideodectest.h
@@ -53,7 +53,7 @@ typedef struct appPrivateType{
   OMX_HANDLETYPE fbdev_sink_handle;
 }appPrivateType;
 
-#define BUFFER_IN_SIZE 4096 
+#define BUFFER_IN_SIZE 8192 
 
 /** Specification version*/
 #define VERSIONMAJOR    1

17c61b6e24aa0b88b2f521c5e25dfeda898e87e6
Pankaj Sen <pankaj.sen@st.com>
2008-01-28 10:15:02 +0000

    Aligned tab/spaces
    git-svn-id: https://omxil.svn.sourceforge.net/svnroot/omxil/libomxil/trunk@464 d11a7513-620d-0410-9d76-e80dcb6a9808

--- test/components/audio/omxaudiodectest.h
+++ test/components/audio/omxaudiodectest.h
@@ -1,32 +1,32 @@
 /**
-	@file test/components/testaudio/omx_audiodectest.h
-	
-	Test application that uses three OpenMAX components, a file reader,an audio decoder 
-	and an alsa sink. The application receives an compressed audio stream on input port
-	from a file, decodes it and sends it to the alsa sink, or to a file or standard output.
-	The audio formats supported are:
-	mp3 (ffmpeg)
-	
-	Copyright (C) 2007  STMicroelectronics and Nokia
+  @file test/components/testaudio/omx_audiodectest.h
+  
+  Test application that uses three OpenMAX components, a file reader,an audio decoder 
+  and an alsa sink. The application receives an compressed audio stream on input port
+  from a file, decodes it and sends it to the alsa sink, or to a file or standard output.
+  The audio formats supported are:
+  mp3 (ffmpeg)
+  
+  Copyright (C) 2007  STMicroelectronics and Nokia
 
-	This library is free software; you can redistribute it and/or modify it under
-	the terms of the GNU Lesser General Public License as published by the Free
-	Software Foundation; either version 2.1 of the License, or (at your option)
-	any later version.
+  This library is free software; you can redistribute it and/or modify it under
+  the terms of the GNU Lesser General Public License as published by the Free
+  Software Foundation; either version 2.1 of the License, or (at your option)
+  any later version.
 
-	This library is distributed in the hope that it will be useful, but WITHOUT
-	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-	FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
-	details.
+  This library is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+  details.
 
-	You should have received a copy of the GNU Lesser General Public License
-	along with this library; if not, write to the Free Software Foundation, Inc.,
-	51 Franklin St, Fifth Floor, Boston, MA
-	02110-1301  USA
-	
-	$Date$
-	Revision $Rev$
-	Author $Author$
+  You should have received a copy of the GNU Lesser General Public License
+  along with this library; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin St, Fifth Floor, Boston, MA
+  02110-1301  USA
+  
+  $Date$
+  Revision $Rev$
+  Author $Author$
 */
 
 #ifndef __OMXAUDIODECTEST_H__
@@ -47,14 +47,14 @@
 #include <user_debug_levels.h>
 
 typedef struct appPrivateType{
-	tsem_t* filereaderEventSem;
-	tsem_t* decoderEventSem;
-	tsem_t* sinkEventSem;
+  tsem_t* filereaderEventSem;
+  tsem_t* decoderEventSem;
+  tsem_t* sinkEventSem;
   tsem_t* volumeEventSem;
-	tsem_t* eofSem;
-	OMX_HANDLETYPE audiodechandle;
-	OMX_HANDLETYPE audiosinkhandle;
-	OMX_HANDLETYPE filereaderhandle;
+  tsem_t* eofSem;
+  OMX_HANDLETYPE audiodechandle;
+  OMX_HANDLETYPE audiosinkhandle;
+  OMX_HANDLETYPE filereaderhandle;
   OMX_HANDLETYPE volumehandle;
 }appPrivateType;
 
@@ -69,66 +69,66 @@ typedef struct appPrivateType{
 
 /* Callback prototypes */
 OMX_ERRORTYPE audiodecEventHandler(
-	OMX_OUT OMX_HANDLETYPE hComponent,
-	OMX_OUT OMX_PTR pAppData,
-	OMX_OUT OMX_EVENTTYPE eEvent,
-	OMX_OUT OMX_U32 Data1,
-	OMX_OUT OMX_U32 Data2,
-	OMX_OUT OMX_PTR pEventData);
+  OMX_OUT OMX_HANDLETYPE hComponent,
+  OMX_OUT OMX_PTR pAppData,
+  OMX_OUT OMX_EVENTTYPE eEvent,
+  OMX_OUT OMX_U32 Data1,
+  OMX_OUT OMX_U32 Data2,
+  OMX_OUT OMX_PTR pEventData);
 
 OMX_ERRORTYPE audiodecEmptyBufferDone(
-	OMX_OUT OMX_HANDLETYPE hComponent,
-	OMX_OUT OMX_PTR pAppData,
-	OMX_OUT OMX_BUFFERHEADERTYPE* pBuffer);
+  OMX_OUT OMX_HANDLETYPE hComponent,
+  OMX_OUT OMX_PTR pAppData,
+  OMX_OUT OMX_BUFFERHEADERTYPE* pBuffer);
 
 OMX_ERRORTYPE audiodecFillBufferDone(
-	OMX_OUT OMX_HANDLETYPE hComponent,
-	OMX_OUT OMX_PTR pAppData,
-	OMX_OUT OMX_BUFFERHEADERTYPE* pBuffer);
+  OMX_OUT OMX_HANDLETYPE hComponent,
+  OMX_OUT OMX_PTR pAppData,
+  OMX_OUT OMX_BUFFERHEADERTYPE* pBuffer);
 
 OMX_ERRORTYPE audiosinkEventHandler(
-	OMX_OUT OMX_HANDLETYPE hComponent,
-	OMX_OUT OMX_PTR pAppData,
-	OMX_OUT OMX_EVENTTYPE eEvent,
-	OMX_OUT OMX_U32 Data1,
-	OMX_OUT OMX_U32 Data2,
-	OMX_OUT OMX_PTR pEventData);
+  OMX_OUT OMX_HANDLETYPE hComponent,
+  OMX_OUT OMX_PTR pAppData,
+  OMX_OUT OMX_EVENTTYPE eEvent,
+  OMX_OUT OMX_U32 Data1,
+  OMX_OUT OMX_U32 Data2,
+  OMX_OUT OMX_PTR pEventData);
 
 OMX_ERRORTYPE audiosinkEmptyBufferDone(
-	OMX_OUT OMX_HANDLETYPE hComponent,
-	OMX_OUT OMX_PTR pAppData,
-	OMX_OUT OMX_BUFFERHEADERTYPE* pBuffer);
-	
+  OMX_OUT OMX_HANDLETYPE hComponent,
+  OMX_OUT OMX_PTR pAppData,
+  OMX_OUT OMX_BUFFERHEADERTYPE* pBuffer);
+  
 OMX_ERRORTYPE filereaderEventHandler(
-	OMX_OUT OMX_HANDLETYPE hComponent,
-	OMX_OUT OMX_PTR pAppData,
-	OMX_OUT OMX_EVENTTYPE eEvent,
-	OMX_OUT OMX_U32 Data1,
-	OMX_OUT OMX_U32 Data2,
-	OMX_OUT OMX_PTR pEventData);
-	
+  OMX_OUT OMX_HANDLETYPE hComponent,
+  OMX_OUT OMX_PTR pAppData,
+  OMX_OUT OMX_EVENTTYPE eEvent,
+  OMX_OUT OMX_U32 Data1,
+  OMX_OUT OMX_U32 Data2,
+  OMX_OUT OMX_PTR pEventData);
+  
 OMX_ERRORTYPE filereaderFillBufferDone(
-	OMX_OUT OMX_HANDLETYPE hComponent,
-	OMX_OUT OMX_PTR pAppData,
-	OMX_OUT OMX_BUFFERHEADERTYPE* pBuffer);
+  OMX_OUT OMX_HANDLETYPE hComponent,
+  OMX_OUT OMX_PTR pAppData,
+  OMX_OUT OMX_BUFFERHEADERTYPE* pBuffer);
 
 OMX_ERRORTYPE volumeEventHandler(
-	OMX_OUT OMX_HANDLETYPE hComponent,
-	OMX_OUT OMX_PTR pAppData,
-	OMX_OUT OMX_EVENTTYPE eEvent,
-	OMX_OUT OMX_U32 Data1,
-	OMX_OUT OMX_U32 Data2,
-	OMX_OUT OMX_PTR pEventData);
+  OMX_OUT OMX_HANDLETYPE hComponent,
+  OMX_OUT OMX_PTR pAppData,
+  OMX_OUT OMX_EVENTTYPE eEvent,
+  OMX_OUT OMX_U32 Data1,
+  OMX_OUT OMX_U32 Data2,
+  OMX_OUT OMX_PTR pEventData);
 
 OMX_ERRORTYPE volumeEmptyBufferDone(
-	OMX_OUT OMX_HANDLETYPE hComponent,
-	OMX_OUT OMX_PTR pAppData,
-	OMX_OUT OMX_BUFFERHEADERTYPE* pBuffer);
+  OMX_OUT OMX_HANDLETYPE hComponent,
+  OMX_OUT OMX_PTR pAppData,
+  OMX_OUT OMX_BUFFERHEADERTYPE* pBuffer);
 
 OMX_ERRORTYPE volumeFillBufferDone(
-	OMX_OUT OMX_HANDLETYPE hComponent,
-	OMX_OUT OMX_PTR pAppData,
-	OMX_OUT OMX_BUFFERHEADERTYPE* pBuffer);
+  OMX_OUT OMX_HANDLETYPE hComponent,
+  OMX_OUT OMX_PTR pAppData,
+  OMX_OUT OMX_BUFFERHEADERTYPE* pBuffer);
 
 #endif
 

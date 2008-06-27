feb912b35c975043d37ea0c844e5504c95854213
Benoit Fouet <benoit.fouet@purplelabs.com>
2008-06-27 11:41:39 +0000

    Simplify DEBUG macro.
    git-svn-id: https://omxil.svn.sourceforge.net/svnroot/omxil/libomxil/trunk@556 d11a7513-620d-0410-9d76-e80dcb6a9808

--- src/omx_comp_debug_levels.h
+++ src/omx_comp_debug_levels.h
@@ -69,9 +69,9 @@
 /** \def DEBUG_LEVEL is the current level do debug output on standard err */
 #define DEBUG_LEVEL (DEB_LEV_ERR)
 #if DEBUG_LEVEL > 0
-#define DEBUG(n, args...) do { if (DEBUG_LEVEL & (n)){fprintf(stderr, "OMX-"); fprintf(stderr, args);} } while (0)
+#define DEBUG(n, fmt, args...) do { if (DEBUG_LEVEL & (n)){fprintf(stderr, "OMX-" fmt, ##args);} } while (0)
 #else
-#define DEBUG(n, args...) 
+#define DEBUG(n, fmt, args...) 
 #endif
 
 #endif
